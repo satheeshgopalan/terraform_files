c76517f564224a4254d95bc1a77b7ed3edcbbc7c
Satheesh Gopalan <satheeshgopalans@gmail.com>
2023-05-03 17:29:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 
 provider "linode" {
-  token = "ea3a224daa3701deaf1a956feae3bb22cfbf5af69cd7d17f3980fc0f93b68d7f"
+  token = "$API_TOKEN" # Update this
 }
 
 resource "linode_instance" "my_instance1" {
@@ -17,7 +17,7 @@ resource "linode_instance" "my_instance1" {
   type = "g6-standard-2"
   region = "us-east"
   image = "linode/Fedora38"
-  root_pass = "Satheesh123"
+  root_pass = "$PASSWORD" #update
 }
 
 resource "linode_instance" "my_instance2" {
@@ -25,7 +25,7 @@ resource "linode_instance" "my_instance2" {
   type = "g6-standard-2"
   region = "ap-west"
   image = "linode/debian10"
-  root_pass = "Satheesh123"
+  root_pass = "$PASSWORD" #update 
 }
 
 resource "linode_volume" "my_storage" {
